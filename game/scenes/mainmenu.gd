fb5ac3259eb5d446e9735c287426f33885a5a52e
Nitya Desaraju <nitya.desaraju@gmail.com>
2026-02-01 20:57:47 +0800

    menu and opening

--- game/scenes/mainmenu.gd
+++ game/scenes/mainmenu.gd
@@ -1,23 +1,10 @@
 extends Node2D
 
-@onready var help = $help
 @onready var start = $start
-@onready var popup = $popup
-@onready var overlay = $overlay
 @onready var anim_player = $AnimationPlayer
 
 func _ready():
-	help.pressed.connect(_on_help_pressed)
 	start.pressed.connect(_on_start_pressed)
-	
-func _input(event):
-	if event is InputEventMouseButton and overlay.visible:
-		popup.hide()
-		overlay.hide()
-
-func _on_help_pressed():
-	popup.show()
-	overlay.show()
 	
 func _on_start_pressed():
 	anim_player.play("fade") 
--- game/scenes/opening.gd
+++ game/scenes/opening.gd
@@ -7,18 +7,21 @@ extends Node2D
 var lines: Array[String] = [
 	"Welcome! If you can't tell, I'm the Grim Reaper. I have hired you to be my assistant!",
 	"I will send you souls, and you have to decide whether to send them to heaven or hell based on their life.",
-	"If you're good at your job, there will be more details you have to check for.",
-	"So, make sure to check your rulebook often for new rules.",
+	"If you're good at your job, there will be more details you have to check for...",
+	"So make sure to check your rulebook often for new rules!",
 	"Ready for your first day?"
 ]
 
 var current_line_index: int = 0
 var is_typing: bool = false
+const MAX_FONT_SIZE: int = 50
+const MIN_FONT_SIZE: int = 12
 
 func _ready():
 	dialogue_label.visible_ratio = 0 
 	anim_player.play_backwards("fade") 
 	await anim_player.animation_finished
+	dialogue_box.show()
 	update_dialogue()
 
 func _input(event):
@@ -35,13 +38,39 @@ func update_dialogue():
 func display_text(text_to_show: String):
 	is_typing = true
 	dialogue_label.text = text_to_show
-	dialogue_label.visible_ratio = 0
 	
+	# Run the scaling logic before starting the tween
+	_rescale_text()
+	
+	dialogue_label.visible_ratio = 0
 	var tween = create_tween()
 	tween.tween_property(dialogue_label, "visible_ratio", 1.0, 1.0)
 	
 	await tween.finished
 	is_typing = false
+
+func _rescale_text():
+	var font = dialogue_label.get_theme_font("font")
+	var font_size = MAX_FONT_SIZE
+	var box_width = dialogue_label.size.x
+	var box_height = dialogue_label.size.y
+	
+	# Loop until the text fits or we hit the minimum size
+	while font_size > MIN_FONT_SIZE:
+		# Calculate how high the text will be at this specific font size
+		var text_height = font.get_multiline_string_size(
+			dialogue_label.text, 
+			HORIZONTAL_ALIGNMENT_LEFT, 
+			box_width, 
+			font_size
+		).y
+		
+		if text_height <= box_height:
+			break # It fits!
+			
+		font_size -= 2 # Shrink and try again
+	
+	dialogue_label.add_theme_font_size_override("font_size", font_size)
 
 func advance_dialogue():
 	current_line_index += 1
