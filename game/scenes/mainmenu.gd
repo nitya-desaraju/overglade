eb4b5a7f98d167e71a301a00374f5a12ebe3a265
Nitya Desaraju <nitya.desaraju@gmail.com>
2026-02-02 02:11:44 +0800

    fixing menu button

--- game/scenes/mainmenu.gd
+++ game/scenes/mainmenu.gd
@@ -1,14 +1,23 @@
 extends Node2D
 
 @onready var start = $start
+@onready var label = $start/RichTextLabel
 @onready var anim_player = $AnimationPlayer
 
 func _ready():
 	anim_player.play_backwards("fade") 
 	await anim_player.animation_finished
 	start.pressed.connect(_on_start_pressed)
+	start.mouse_entered.connect(_on_start_hovered)
+	start.mouse_exited.connect(_on_start_unhovered)
 	
 func _on_start_pressed():
 	anim_player.play("fade") 
 	await anim_player.animation_finished
 	get_tree().change_scene_to_file("res://scenes/opening.tscn")
+
+func _on_start_hovered():
+	label.position.y = 52
+
+func _on_start_unhovered():
+	label.position.y = 25
